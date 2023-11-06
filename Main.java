0b53ade74d8cf50453e4ce7e7b9b27d1af2738d5
MrDemkoko <mr.demkoko@gmail.com>
2023-11-06 14:04:19 +0200

    v1.1 Now the game tells you on which try have you guessed

--- Main.java
+++ Main.java
@@ -5,15 +5,18 @@ public class Main {
     public static void main(String[] args) {
         
         int RandomNumber = new Random().nextInt(10);
+        int GuessCount = 0;
         System.out.println("Guess the number from 0 to 9 :3");
         
         while (true) {
 
             System.out.println("Type your guess:");
             int GuessNumber = new Scanner(System.in).nextInt();
+            ++GuessCount;
 
             if (GuessNumber == RandomNumber) {
                 System.out.println("Wow! You have guessed the number correctly!");
+                System.out.println("You have guessed on the " + GuessCount + " try");
                 break;
             } else {
                 System.out.println("You didn\'t guess the number right(( Try again...");
