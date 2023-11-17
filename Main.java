cda99730dad968785cead956fc484d1ec221991f
MrDemkoko <mr.demkoko@gmail.com>
2023-11-17 21:14:11 +0200

    Everything changed, README.md added

--- Main.java
+++ Main.java
@@ -4,24 +4,66 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
-        int RandomNumber = new Random().nextInt(10);
-        int GuessCount = 0;
-        System.out.println("Guess the number from 0 to 9 :3");
-        
-        while (true) {
+        System.out.println("\t*Hey! This is a number guessing game*\n");
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Type your guess:");
-            int GuessNumber = new Scanner(System.in).nextInt();
-            ++GuessCount;
+        for(;;) {
 
-            if (GuessNumber == RandomNumber) {
-                System.out.println("Wow! You have guessed the number correctly!");
-                System.out.println("You have guessed on the " + GuessCount + " try");
-                break;
-            } else {
-                System.out.println("You didn\'t guess the number right(( Try again...");
+            System.out.println("***\n1 - Play\n0 - Quit\n***");
+
+            if(scanner.hasNextInt()) {
+
+                int menuOption = scanner.nextInt();
+
+                if(menuOption == 1) {
+
+                    System.out.println("Enter the range of numbers you want to guess from:");
+
+                    if(scanner.hasNextInt()) {
+
+                        int rangeInput = scanner.nextInt();
+
+                        if(rangeInput <= 0) {
+                            System.out.println("Invalid range: enter value greater than 0");
+                            return;
+                        }
+
+                        Random random = new Random();
+                        int randomNumber = random.nextInt(rangeInput + 1);
+
+                        System.out.println("Great! Guess the number from 0 to " + rangeInput);
+
+                        for(;;) {
+
+                            System.out.println("Enter your guess:");
+                            if(scanner.hasNextInt()) {
+
+                                int userGuess = scanner.nextInt();
+                                if (userGuess == randomNumber) {
+                                    System.out.println("You have guessed the number! Congrats!");
+                                    break;
+                                } else {
+                                    System.out.println("Wrong one( Try again");
+                                }
+
+                            } else {
+                                System.out.println("Invalid input: not a number");
+                                scanner.next();
+                            }
+
+                        }
+
+                    } else {
+                        System.out.println("Invalid input: not a number");
+                        scanner.next();
+                    }
+                } else if(menuOption == 0) {
+                    break;
+                } else {
+                    System.out.println("Invalid input: not an option");
+                }
             }
-
         }
+        scanner.close();
     }
 }
---
+++ README.md
@@ -0,0 +1,23 @@
+# Guess The Number game 
+
+## Requirements
+1. Have JDK installed on your machine
+2. Have Git installed on your machine
+
+## Installation&&Run
+1. Clone the repository
+```bash
+$ git clone <https://github.com/MrDemkoko/GuessNumGame>
+```
+2. Go to the project's directory
+```bash
+$ cd GuessNumGame
+```
+3. Compile the code
+```bash
+$ javac Main.java
+```
+4. Run
+```bash
+$ java Main Main.class
+```
